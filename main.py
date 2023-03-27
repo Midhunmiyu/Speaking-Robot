a768771842ad889864c0bfb5af564a7b4d4c6f40
Midhunmiyu <midhunrajagopal112@gmail.com>
2023-03-26 20:33:36 -0700

    second commit

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ import win32com.client as wincom
 speak = wincom.Dispatch("SAPI.SpVoice")
 print("Welcome To Midhun's Kingdom")
 print("Speaking Robo 2.0 Version is Created...")
-text = input("Enter what you want to hear from Robo")
+text = input("Enter what you want to hear from Robo : ")
 speak.Speak(text)
 time.sleep(3)
 text='Do you want me to speak more??'
 speak.Speak(text)
-text = input("Enter what you want to hear from Robo")
-speak.Speak(text)
+while True:
+    if text == 'q':
+        break
+    text = input("Enter what you want to hear from Robo : ")
+    speak.Speak(text)
 
 # # this code only work in windows machine
 # time.sleep(3) is used to give a gap/pause in speaking,where 3s is the time gap
